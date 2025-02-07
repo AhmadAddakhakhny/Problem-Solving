332e45b8be9eefcdf050cbdb84088b2044f1b960
AhmadAddakhakhny <114470710+AhmadAddakhakhny@users.noreply.github.com>
2025-02-07 11:27:24 +0200

    Update missingNumber.cpp
    support more safe implementation to avoid overflow

--- missingNumber.cpp
+++ missingNumber.cpp
@@ -1,19 +1,28 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
 int missingNumber(vector<int>& nums) {
-  int sz = nums.size();
-  int great_sum = (sz*(sz+1)) / 2;
-  int total = 0;
-  for(auto it : nums) {
-    total += it;
+  int sum = nums.size();
+
+  /* this technique prevents overflow for large numbers summation */
+  for (int idx = 0; idx < nums.size(); idx++) {
+    sum += idx - nums[idx];
   }
-
-  int sub = great_sum - total;
-
-  return sub;
+  
+  return sum;
 }
+// int missingNumber(vector<int>& nums) {
+//   int sz = nums.size();
+//   int great_sum = (sz*(sz+1)) / 2;
+//   int total = 0;
+//   for(auto it : nums) {
+//     total += it;
+//   }
+
+//   int sub = great_sum - total;
+
+//   return sub;
+// }
 
 int main(){
   vector<int> v{0,1,2};
