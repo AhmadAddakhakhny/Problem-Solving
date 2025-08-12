fb63d433711f3e9d526d793c0e7dbe1d50601538
Dakhakhny <ahmed.addakhakhny@gmail.com>
2025-08-12 09:28:53 +0200

    Solve a fixed sliding window problem

--- 007_containsDuplicate.cpp
+++ 007_containsDuplicate.cpp
@@ -1,25 +1,47 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+// LeetCode 219 - Contains Duplicate II
+// Adjust the below window technique, and also solve it using  map of <value, index> 
 
-bool containsDuplicate(vector<int>& nums) {
-  map<int, bool> freqMp;
-  for(const auto &it : nums) {
-    if(!freqMp.count(it)) {
-      freqMp.insert(std::pair<int,bool>(it, true));
-    } else {
-      return true;
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    map<int, bool> mp;
+    bool status = false;
+    k++;
+
+    for (int i = 0; i < k && !status && i < nums.size(); i++) {
+        status = mp.count(nums[i]);
+        if (!status) {
+            mp[nums[i]] = true;
+        }        
     }
-  }
-  return false;
+
+    if(!status) {
+        for (int start = k; start < nums.size() && !status && start < nums.size(); start ++) {
+            // remove first emlement
+            mp.erase(nums[start-k]);
+            // check if the element to be inserted existed
+            status = mp.count(nums[start]);
+            // add new element
+            if (!status) {
+                mp[nums[start]] = true;
+            }       
+        }
+    }
+
+    return status;
 }
+int main () {
 
-int main(){
+    // vector<int> nums{1,2,3,1};
+    // int k = 3; // Output: true
+    // vector<int> nums{1,0,1,1};
+    // int k = 1;   // Output: true   
+    // vector<int> nums{1,2,3,1,2,3};
+    // int k = 2;   // Output: false   
+    vector<int> nums{1,2,3,4,5,6,7,8,9,9};
+    int k = 3;   // Output: false 
 
-  vector<int> v1{1,2,3,1};
-  vector<int> v2{1,2,3,1};
-
-  cout << "V1 expect Fales == " << containsDuplicate(v1) << std::endl;
-  cout << "V2 expect true == " << containsDuplicate(v2) << std::endl;
-  return 0;
+    cout << containsNearbyDuplicate(nums,k) << std::endl;
+    return 0;
 }
